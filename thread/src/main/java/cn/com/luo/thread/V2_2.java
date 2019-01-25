d9902e274d53740f0005ba53b686eb2c9ce9c54d
luozhaohuan <luo.zhaohuan@puscene.com>
2019-01-25 20:11:28 +0800

    wait 对象改变 锁不会跟着变

--- thread/src/main/java/cn/com/luo/thread/V2_2.java
+++ thread/src/main/java/cn/com/luo/thread/V2_2.java
@@ -10,28 +10,47 @@ public class V2_2 extends Thread{
     private static  Boolean BOOLEAN = true;
 
     public void run() {
-        synchronized (BOOLEAN){
+        synchronized (V2_2.class){
             if(BOOLEAN){
                 try {
                     System.out.println("wait start");
-                    BOOLEAN.wait();
+                    V2_2.class.wait();
                     System.out.println("wait end");
                 } catch (InterruptedException e) {
 
                 }
             }else {
                 System.out.println("notify start");
-                BOOLEAN.notify();
+                V2_2.class.notify();
                 System.out.println("notify end");
             }
         }
     }
+    // 以BOOLEAN为锁，BOOLEAN重新赋值后为一个新的对象，所以无法唤醒BOOLEAN.wait()等待
+//    public void run() {
+////        synchronized (BOOLEAN){
+////            if(BOOLEAN){
+////                try {
+////                    System.out.println("wait start");
+////                    BOOLEAN.wait();
+////                    System.out.println("wait end");
+////                } catch (InterruptedException e) {
+////
+////                }
+////            }else {
+////                System.out.println("notify start");
+////                BOOLEAN.notify();
+////                System.out.println("notify end");
+////            }
+////        }
+////    }
 
 
     public static void main(String[] args) throws InterruptedException {
         V2_2 v2_2 = new V2_2();
         V2_2 v2_3 = new V2_2();
         v2_2.start();
+        sleep(10);
         BOOLEAN = false;
         v2_3.start();
 
